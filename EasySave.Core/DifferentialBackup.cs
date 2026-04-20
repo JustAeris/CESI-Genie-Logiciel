868be1d9d8bac91abaa054a5cd47137b41329b61
matth <aeris.github@gmail.com>
2026-04-20 15:03:57 +0200

    feat(core): add DifferentialBackup strategy

---
+++ EasySave.Core/DifferentialBackup.cs
@@ -0,0 +1,35 @@
+namespace EasySave.Core;
+
+public class DifferentialBackup : BackupStrategyBase, IBackupStrategy
+{
+    /// <summary>Copies only files that are new or modified since the last backup.</summary>
+    public void Execute(BackupJob job, BackupState state)
+    {
+        var allFiles = Directory.GetFiles(job.SourceDir, "*", SearchOption.AllDirectories);
+
+        var filesToCopy = allFiles.Where(src =>
+        {
+            var dst = BuildDestPath(src, job.SourceDir, job.TargetDir);
+            // Only copy if destination doesn't exist or source is newer
+            bool needsCopy = !File.Exists(dst) || File.GetLastWriteTime(src) > File.GetLastWriteTime(dst);
+            return needsCopy;
+        }).ToArray();
+
+        state.TotalFilesToCopy = filesToCopy.Length;
+        state.TotalFilesSize = filesToCopy.Sum(f => new FileInfo(f).Length);
+        state.NbFilesLeftToDo = filesToCopy.Length;
+        state.SizeLeft = state.TotalFilesSize;
+        state.State = "ACTIVE";
+
+        foreach (var src in filesToCopy)
+        {
+            var dst = BuildDestPath(src, job.SourceDir, job.TargetDir);
+            CopyFile(src, dst, state);
+        }
+
+        state.State = "END";
+        state.SourceFilePath = "";
+        state.TargetFilePath = "";
+        StateManager.Instance.Update(state);
+    }
+}
