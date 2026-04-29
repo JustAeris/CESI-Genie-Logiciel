6eab55afbd2c240a02314e113077c66892668034
mapuce107 <emmanuelamessanga@icloud.com>
2026-04-29 06:36:26 +0200

    Implement CryptoSoftRunner

--- EasySave.Core/CryptoSoftRunner.cs
+++ EasySave.Core/CryptoSoftRunner.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace EasySave.Core;
 
-namespace EasySave.Core
+public class CryptoSoftRunner : ICryptoService
 {
-    internal class CryptoSoftRunner
+    // Run cryptosoft.exe and return execution time in ms (negative = error)
+    public long Encrypt(string filePath)
     {
+        try
+        {
+            var startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "cryptosoft.exe",
+                Arguments = filePath,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+            var process = System.Diagnostics.Process.Start(startInfo);
+            process?.WaitForExit();
+
+            stopwatch.Stop();
+
+            // Exit code > 0 = success, < 0 = error
+            if (process?.ExitCode > 0)
+                return stopwatch.ElapsedMilliseconds;
+            else
+                return -1;
+        }
+        catch
+        {
+            return -1;
+        }
     }
 }
